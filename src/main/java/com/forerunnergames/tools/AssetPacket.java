6d33083b5bd34dcf9bccdaa947868d81ca379b0b
Aaron Mahan <aaron@forerunnergames.com>
2013-12-04 16:30:49 -0900

    Add NetworkPacket

--- src/main/java/com/forerunnergames/tools/AssetPacket.java
+++ src/main/java/com/forerunnergames/tools/AssetPacket.java
@@ -1,7 +1,7 @@
 // Copyright © 2011 - 2013 Forerunner Games. All rights reserved.
 package com.forerunnergames.tools;
 
-public interface AssetPacket
+public interface AssetPacket extends NetworkPacket
 {
   public String getName();
   public int getId();

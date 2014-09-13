d2d9328fb7c501f48563215895d6e9ebf07c8df7
Aaron Mahan <aaron@forerunnergames.com>
2014-09-12 20:58:42 -0800

    Add IterableEnum & IterableEnumHelper.

---
+++ src/main/java/com/forerunnergames/tools/IterableEnum.java
@@ -0,0 +1,12 @@
+package com.forerunnergames.tools;
+
+public interface IterableEnum <E extends Enum <E>>
+{
+  public boolean hasNext();
+  public E next();
+  public boolean hasPrevious();
+  public E previous();
+  public boolean is (E e);
+  public int getPosition();
+  public String toMixedOrdinalPosition();
+}

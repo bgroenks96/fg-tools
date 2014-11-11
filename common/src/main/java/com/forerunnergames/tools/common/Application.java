eaec33755d31e016bed5ee66330e74b18d9bb0b0
Aaron Mahan <aaron@forerunnergames.com>
2014-11-11 08:02:40 -0900

    Add update method to Application.

--- common/src/main/java/com/forerunnergames/tools/common/Application.java
+++ common/src/main/java/com/forerunnergames/tools/common/Application.java
@@ -5,6 +5,7 @@ public interface Application
   public void initialize();
   public void add (final Controller controller);
   public void remove (final Controller controller);
+  public void update();
   public void shutDown();
   public boolean shouldShutDown();
 }

c42be43cb7d220250b15728581bec4269dc2d9f5
Aaron Mahan <aaron@forerunnergames.com>
2014-11-11 07:44:30 -0900

    Make Application add/remove Controller's.

--- common/src/main/java/com/forerunnergames/tools/common/Application.java
+++ common/src/main/java/com/forerunnergames/tools/common/Application.java
@@ -3,6 +3,8 @@ package com.forerunnergames.tools.common;
 public interface Application
 {
   public void initialize();
+  public void add (final Controller controller);
+  public void remove (final Controller controller);
   public void shutDown();
   public boolean shouldShutDown();
 }

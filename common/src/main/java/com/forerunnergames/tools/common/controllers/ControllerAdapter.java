3a681877d852309caace23ae10cf2f2da319940f
Aaron Mahan <aaron@forerunnergames.com>
2014-11-11 08:10:15 -0900

    Create ControllerAdapter.

---
+++ common/src/main/java/com/forerunnergames/tools/common/controllers/ControllerAdapter.java
@@ -0,0 +1,25 @@
+package com.forerunnergames.tools.common.controllers;
+
+public class ControllerAdapter implements Controller
+{
+  @Override
+  public void initialize()
+  {
+  }
+
+  @Override
+  public void update()
+  {
+  }
+
+  @Override
+  public boolean shouldShutDown()
+  {
+    return false;
+  }
+
+  @Override
+  public void shutDown()
+  {
+  }
+}

58a8c9b45736852e76f4d9180d4b6d3ec6d297ea
Aaron Mahan <aaron@forerunnergames.com>
2014-11-23 21:46:04 -0900

    Major Randomness overhaul.
    Set debug or release mode to use either a system-based entropy source or
    remote HotBits (nuclear) entropy source, respectively. This is slightly
    better than selectively commenting out code ;-).
    
    Keep track of how many times the random number generator was used, and
    reseed when it exceeds the threshold.
    
    Automatically fall back to a system-based entropy source if anything
    goes wrong obtaining remote HotBits entropy. Log a warning when this
    occurs, explaining what went wrong.
    
    Refactor out the number of seed bytes as a configurable constant.
    
    Major style overhaul, complete with a ton of refactoring to eliminate
    duplication and enhance clarity.
    
    Add some extra javadoc and comments for even more clarity.

--- common/src/main/java/com/forerunnergames/tools/common/Randomness.java
+++ common/src/main/java/com/forerunnergames/tools/common/Randomness.java
@@ -1,5 +1,6 @@
-// Copyright © 2011 - 2013 Forerunner Games. All rights reserved.
 package com.forerunnergames.tools.common;
+
+import com.forerunnergames.tools.randomx.RandomHotBits;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
@@ -7,65 +8,140 @@ import java.security.SecureRandom;
 import java.util.Collections;
 import java.util.List;
 
-//import com.forerunnergames.tools.randomx.RandomHotBits;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Static Utility Class for Random Number-related Functionality
- *
- * @author Aaron Mahan <aaron@forerunnergames.com>
+ * Utility class for random-number-related functionality
  */
 public final class Randomness
 {
-  // Initialize the cryptographically secure pseudo random number generator.
+  private static final Logger log = LoggerFactory.getLogger (Randomness.class);
+  private static final int SEED_BYTES = 8; // The number of bytes used to seed the pseudo random number generator.
+  private static final int RESEED_THRESHOLD = 10000; // The number of random numbers that can be generated before reseeding.
+  private static final RandomHotBits TRNG = new RandomHotBits(); // True random number generator, used for seeding only, and only in RELEASE mode.
+  private static long reseedCounter = 0; // Keeps track of how many random numbers have been generated for reseeding purposes.
+  private static SecureRandom prng; // Cryptographically secure pseudo random number generator.
+  private static Mode currentMode;
+
+  /**
+   * The default mode is DEBUG.
+   */
+  public enum Mode
+  {
+    /**
+     * Uses system entropy to seed a cryptographically-secure random number generator.
+     */
+    DEBUG,
+
+    /**
+     * Uses HotBits entropy (https://www.fourmilab.ch/hotbits) to seed a cryptographically-secure random number
+     * generator. WARNING: HotBits will attempt to connect to and download the entropy from a remote server,
+     * so internet access is required, and the remote server must be online & functioning. If obtaining HotBits
+     * entropy fails, it will automatically fall back to using system entropy, with a log entry explaining what
+     * went wrong.
+     */
+    RELEASE
+  }
+
+  // Initialize the random number generator.
   static
   {
-    // Begin production code
-    /*
-    final RandomHotBits trng = new RandomHotBits();
+    setModeTo (Mode.DEBUG);
+  }
 
-    final int NUMBER_OF_SEED_BYTES = 8;
+  /**
+   * Checks whether the specified mode is set.
+   *
+   * @param mode, The mode to check, must not be null
+   */
+  public static boolean isMode (final Mode mode)
+  {
+    Arguments.checkIsNotNull (mode, "mode");
 
-    final byte[] trueRandomEntropySeed = new byte [NUMBER_OF_SEED_BYTES];
+    return mode.equals (currentMode);
+  }
 
-    for (int i = 0; i < trueRandomEntropySeed.length; ++i)
+  /**
+   * Sets the specified mode. Idempotent - nothing will happen if the specified mode has already been set.
+   *
+   * @param mode, The mode to set, must not be null.
+   *
+   * @see #isMode
+   */
+  public static void setModeTo (final Mode mode)
+  {
+    Arguments.checkIsNotNull (mode, "mode");
+
+    if (isMode (mode)) return;
+
+    currentMode = mode;
+
+    createPrng();
+
+    switch (mode)
     {
-      trueRandomEntropySeed [i] = trng.nextByte();
-    }
+      case DEBUG:
+      {
+        reseedPrngWithSystemEntropy();
 
+        break;
+      }
+      case RELEASE:
+      {
+        reseedPrngWithHotBitsEntropy();
+
+        break;
+      }
+    }
+  }
+
+  private static void createPrng()
+  {
     try
     {
-      Randomness.prng = SecureRandom.getInstance ("SHA1PRNG", "SUN");
+      prng = SecureRandom.getInstance ("SHA1PRNG", "SUN");
     }
-    catch (NoSuchAlgorithmException e)
+    catch (final NoSuchAlgorithmException | NoSuchProviderException e)
     {
       throw new RuntimeException ("Cannot create random number generator", e);
     }
-    catch (NoSuchProviderException e)
-    {
-      throw new RuntimeException ("Cannot create random number generator", e);
-    }
+  }
 
-    Randomness.prng.setSeed (trueRandomEntropySeed);
-    */
-    // End production code
+  private static void reseedPrngWithSystemEntropy()
+  {
+    prng.setSeed (generateSystemEntropySeed());
+  }
 
-    // Begin testing code
-    // @TESTING
+  private static byte[] generateSystemEntropySeed()
+  {
+    return prng.generateSeed (SEED_BYTES);
+  }
+
+  private static void reseedPrngWithHotBitsEntropy()
+  {
+    prng.setSeed (generateHotBitsEntropySeed (SEED_BYTES));
+  }
+
+  private static byte[] generateHotBitsEntropySeed (final int numBytes)
+  {
     try
     {
-      Randomness.prng = SecureRandom.getInstance ("SHA1PRNG", "SUN");
-    }
-    catch (NoSuchAlgorithmException e)
-    {
-      throw new RuntimeException ("Cannot create random number generator", e);
-    }
-    catch (NoSuchProviderException e)
-    {
-      throw new RuntimeException ("Cannot create random number generator", e);
-    }
+      final byte[] trueRandomEntropySeed = new byte[numBytes];
 
-    Randomness.prng.nextBytes (new byte [8]);
-    // End testing code
+      for (int i = 0; i < trueRandomEntropySeed.length; ++i)
+      {
+        trueRandomEntropySeed[i] = TRNG.nextByte();
+      }
+
+      return trueRandomEntropySeed;
+    }
+    catch (final RuntimeException e)
+    {
+      log.warn ("Could not obtain HotBits entropy! Falling back to a system entropy source", e);
+
+      return generateSystemEntropySeed();
+    }
   }
 
   /**
@@ -73,10 +149,7 @@ public final class Randomness
    * pseudo random number generator.
    *
    * @param inclusiveLowerBound The inclusive lower bound, must be >= 0 and <= inclusiveUpperBound and < Integer.MAX_VALUE.
-   * 
    * @param inclusiveUpperBound The inclusive upper bound, must be >= 0 and >= inclusiveLowerBound and < Integer.MAX_VALUE.
-   *
-   * @return A random integer in the range [inclusiveLowerBound, inclusiveUpperBound].
    */
   public static int getRandomIntegerFrom (int inclusiveLowerBound, int inclusiveUpperBound)
   {
@@ -91,7 +164,11 @@ public final class Randomness
     assert n > 0;
     assert n <= (long) Integer.MAX_VALUE;
 
+    checkPrngUsage();
+
     final long randomNumber = (long) inclusiveLowerBound + prng.nextInt ((int) n);
+
+    updatePrngUsage (1);
 
     assert randomNumber >= inclusiveLowerBound;
     assert randomNumber <= inclusiveUpperBound;
@@ -99,28 +176,60 @@ public final class Randomness
     return (int) randomNumber;
   }
 
+  private static void checkPrngUsage()
+  {
+    if (shouldReseedPrng()) reseedPrng();
+  }
+
+  private static boolean shouldReseedPrng()
+  {
+    return reseedCounter > RESEED_THRESHOLD;
+  }
+
+  private static void reseedPrng()
+  {
+    reseedCounter = 0;
+
+    switch (currentMode)
+    {
+      case DEBUG:
+      {
+        reseedPrngWithSystemEntropy();
+
+        break;
+      }
+      case RELEASE:
+      {
+        reseedPrngWithHotBitsEntropy();
+
+        break;
+      }
+    }
+  }
+
+  private static void updatePrngUsage (final int timesUsed)
+  {
+    reseedCounter += timesUsed;
+  }
+
   /**
-   * Shuffles a list using a cryptographically secure pseudo random number
-   * generator.
+   * Shuffles a list using a cryptographically secure pseudo random number generator.
    * 
    * @param <T>  The element type of the specified list.
    * @param list The list to be shuffled, must not be null.
    * 
    * @return A copy of the shuffled list.
-   * 
-   * @throws IllegalArgumentException If list is null.
    */
   public static <T> List <T> shuffle (final List <T> list)
   {
     Arguments.checkIsNotNull (list, "List");
 
-    assert (Randomness.prng != null);
+    checkPrngUsage();
 
-    Collections.shuffle (list, Randomness.prng);
+    Collections.shuffle (list, prng);
+
+    updatePrngUsage (list.size());
 
     return list;
   }
-
-  // Cryptographically secure pseudo random number generator.
-  private static SecureRandom prng;
 }

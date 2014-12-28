0843313faf6a97979f56b7802e167ca873a19bc3
Aaron Mahan <aaron@forerunnergames.com>
2014-12-27 15:45:36 -0900

    Improve ResultFluency.
    - Add null argument check.
    - Add 'withFailureReasonFrom'.

--- common/src/main/java/com/forerunnergames/tools/common/ResultFluency.java
+++ common/src/main/java/com/forerunnergames/tools/common/ResultFluency.java
@@ -4,7 +4,14 @@ public final class ResultFluency
 {
   public static <T> T failureReasonFrom (final Result <T> result)
   {
+    Arguments.checkIsNotNull (result, "result");
+
     return result.getFailureReason();
+  }
+
+  public static <T> T withFailureReasonFrom (final Result <T> result)
+  {
+    return failureReasonFrom (result);
   }
 
   private ResultFluency()

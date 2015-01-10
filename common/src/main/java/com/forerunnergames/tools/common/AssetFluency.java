8f62357ff9e10f542491e6eed809718592dbb278
Aaron Mahan <aaron@forerunnergames.com>
2015-01-10 02:55:54 -0900

    Add AssetFluency.
    Provides convenience methods for the Asset class that enhance
    readability.

---
+++ common/src/main/java/com/forerunnergames/tools/common/AssetFluency.java
@@ -0,0 +1,37 @@
+package com.forerunnergames.tools.common;
+
+import com.forerunnergames.tools.common.id.Id;
+
+public final class AssetFluency
+{
+  public static String nameOf (final Asset asset)
+  {
+    Arguments.checkIsNotNull (asset, "asset");
+
+    return asset.getName ();
+  }
+
+  public static Id withIdOf (final Asset asset)
+  {
+    return idOf (asset);
+  }
+
+  public static Id idOf (final Asset asset)
+  {
+    Arguments.checkIsNotNull (asset, "asset");
+
+    return asset.getId ();
+  }
+
+  public static int idValueOf (final Asset asset)
+  {
+    Arguments.checkIsNotNull (asset, "asset");
+
+    return asset.getId ().value ();
+  }
+
+  private AssetFluency ()
+  {
+    Classes.instantiationNotAllowed ();
+  }
+}

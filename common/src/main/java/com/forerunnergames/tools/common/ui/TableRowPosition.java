e50474a2e53bc1c02209f6f872dbb08339525657
Aaron Mahan <aaron@forerunnergames.com>
2015-01-22 14:38:02 -0900

    - Delete obsolete TableRowPosition & ui package.
    - TableRowPosition was intended to create a table-based structure from
      scratch intended for use with Nifty Gui, but it is no longer needed or
      being used.
    
    - common::ui package only contains TableRowPosition, so after deleting
      TableRowPosition, it's empty, so delete it.

--- common/src/main/java/com/forerunnergames/tools/common/ui/TableRowPosition.java
+++
@@ -1,7 +0,0 @@
-package com.forerunnergames.tools.common.ui;
-
-import com.forerunnergames.tools.common.enums.IterableEnum;
-
-public interface TableRowPosition <E extends Enum <E> & TableRowPosition <E>> extends IterableEnum <E>
-{
-}

435b5c76d357a41e6a451cc6c7229913cd8a1363
Aaron Mahan <aaron@forerunnergames.com>
2015-06-04 15:16:08 -0800

    Minor refactoring of Client & Server.
    - Rename add (final NetworkListener listener) method parameter
      'listener' to 'networkListener' to add clarity.
    
    - Rename remove (final NetworkListener listener) method parameter
      'listener' to 'networkListener' to add clarity.
    
    - Auto format both interfaces to adhere to project code style.

--- net/src/main/java/com/forerunnergames/tools/net/Client.java
+++ net/src/main/java/com/forerunnergames/tools/net/Client.java
@@ -2,11 +2,11 @@ package com.forerunnergames.tools.net;
 
 public interface Client extends ServerConnector, ServerCommunicator
 {
-  void add (final NetworkListener listener);
+  void add (final NetworkListener networkListener);
 
-  void remove (final NetworkListener listener);
+  void remove (final NetworkListener networkListener);
 
-  void register (final Class<?> type);
+  void register (final Class <?> type);
 
   void start ();
 
--- net/src/main/java/com/forerunnergames/tools/net/Server.java
+++ net/src/main/java/com/forerunnergames/tools/net/Server.java
@@ -2,11 +2,11 @@ package com.forerunnergames.tools.net;
 
 public interface Server extends ClientConnector, ClientCommunicator
 {
-  void add (final NetworkListener listener);
+  void add (final NetworkListener networkListener);
 
-  void remove (final NetworkListener listener);
+  void remove (final NetworkListener networkListener);
 
-  void register (final Class<?> type);
+  void register (final Class <?> type);
 
   void start (final int tcpPort);
 

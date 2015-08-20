59f780f9189a8c082bd46cea5b7fd8b0e8015cd8
bgroenks96 <brian@forerunnergames.com>
2015-08-20 19:58:18 -0400

    Add request type getter to ResponseRequestEvent.
    This allows us to circumvent single dispatch for response-request handlers and have access to the ResponseRequestEvent's corresponding ServerRequestEvent type.
    
    Most specifically, the purpose of this change is to prevent having to duplicate code for every type of ResponseRequestEvent in peril's core:MultiplayerController.

--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/client/ResponseRequestEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/client/ResponseRequestEvent.java
@@ -1,6 +1,7 @@
 package com.forerunnergames.tools.net.events.remote.origin.client;
 
 import com.forerunnergames.tools.net.events.remote.ResponseEvent;
+import com.forerunnergames.tools.net.events.remote.origin.server.ServerRequestEvent;
 
 /**
  * A client is responding to a request from the server, while simultaneously requesting a confirmation of said response
@@ -8,4 +9,5 @@ import com.forerunnergames.tools.net.events.remote.ResponseEvent;
  */
 public interface ResponseRequestEvent extends ClientRequestEvent, ResponseEvent
 {
+  Class <? extends ServerRequestEvent> getRequestType ();
 }

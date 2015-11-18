7b34fdc1d33748f3f2af0bf34b81d2418f45e1b1
Aaron Mahan <aaron@forerunnergames.com>
2015-11-17 23:40:49 -0900

    Add Postconditions class.

---
+++ common/src/main/java/com/forerunnergames/tools/common/Postconditions.java
@@ -0,0 +1,41 @@
+package com.forerunnergames.tools.common;
+
+public class Postconditions
+{
+  /**
+   * Checks if the specified boolean condition evaluates to true.
+   *
+   * @param condition
+   *          The boolean condition to check.
+   * @param errorMessage
+   *          The error message to include in the exception.
+   *
+   * @throws IllegalArgumentException
+   *           If condition evaluates to false;
+   */
+  public static void checkIsTrue (final boolean condition, final String errorMessage)
+  {
+    if (!condition) throw new IllegalStateException (errorMessage);
+  }
+
+  /**
+   * Checks if the specified boolean condition evaluates to false.
+   *
+   * @param condition
+   *          The boolean condition to check.
+   * @param errorMessage
+   *          The error message to include in the exception.
+   *
+   * @throws IllegalArgumentException
+   *           If condition evaluates to true;
+   */
+  public static void checkIsFalse (final boolean condition, final String errorMessage)
+  {
+    if (condition) throw new IllegalStateException (errorMessage);
+  }
+
+  private Postconditions ()
+  {
+    Classes.instantiationNotAllowed ();
+  }
+}

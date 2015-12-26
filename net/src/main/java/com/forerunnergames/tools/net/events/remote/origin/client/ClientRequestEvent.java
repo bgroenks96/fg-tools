3f2833fdaec0f43b9f7172ed249b9e192754caa9
Aaron Mahan <aaron@forerunnergames.com>
2015-12-26 01:22:09 -0900

    Fix some javadoc in net package.

--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/client/ClientRequestEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/client/ClientRequestEvent.java
@@ -3,7 +3,7 @@ package com.forerunnergames.tools.net.events.remote.origin.client;
 import com.forerunnergames.tools.net.events.remote.RequestEvent;
 
 /**
- * The server is requesting something from a client.
+ * A client is requesting something from the server.
  *
  * This event is intended to be sent over the network.
  */
--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/ServerEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/ServerEvent.java
@@ -3,7 +3,7 @@ package com.forerunnergames.tools.net.events.remote.origin.server;
 import com.forerunnergames.tools.net.events.remote.RemoteEvent;
 
 /**
- * Marks all events that are sent over the network from the server to the client.
+ * Marks all events that are sent over the network from the server to a client or clients.
  */
 public interface ServerEvent extends RemoteEvent
 {

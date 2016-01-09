152de8458b7ec24815850ce7d5d188263c613b0d
Aaron Mahan <aaron@forerunnergames.com>
2016-01-08 22:01:50 -0900

    Start a server bound to any address.

--- net/src/main/java/com/forerunnergames/tools/net/server/Server.java
+++ net/src/main/java/com/forerunnergames/tools/net/server/Server.java
@@ -4,6 +4,8 @@ import com.forerunnergames.tools.net.NetworkListener;
 import com.forerunnergames.tools.net.client.ClientCommunicator;
 import com.forerunnergames.tools.net.client.ClientConnector;
 
+import java.net.InetSocketAddress;
+
 public interface Server extends ClientConnector, ClientCommunicator
 {
   void add (final NetworkListener networkListener);
@@ -13,6 +15,8 @@ public interface Server extends ClientConnector, ClientCommunicator
   void register (final Class <?> type);
 
   void start (final int tcpPort);
+
+  void start (final InetSocketAddress addressWithTcpPort);
 
   void stop ();
 

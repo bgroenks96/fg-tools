85cce8ad341e83d4c942fbbebc46c5c2e0d501ab
Aaron Mahan <aaron@forerunnergames.com>
2016-05-22 00:46:30 -0800

    Add Constants class.

---
+++ common/src/main/java/com/forerunnergames/tools/common/Constants.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2016 Forerunner Games, LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.forerunnergames.tools.common;
+
+public class Constants
+{
+  public static final float ONE_HALF = 1.0f / 2.0f;
+  public static final float ONE_THIRD = 1.0f / 3.0f;
+  public static final float ONE_FOURTH = 1.0f / 4.0f;
+  public static final float ONE_FIFTH = 1.0f / 5.0f;
+  public static final float ONE_SIXTH = 1.0f / 6.0f;
+  public static final float ONE_SEVENTH = 1.0f / 7.0f;
+  public static final float ONE_EIGHTH = 1.0f / 8.0f;
+  public static final float ONE_NINTH = 1.0f / 9.0f;
+  public static final float ONE_TENTH = 1.0f / 10.0f;
+  public static final float TWO_THIRDS = 2.0f / 3.0f;
+  public static final float TWO_FIFTHS = 2.0f / 5.0f;
+  public static final float TWO_SEVENTHS = 2.0f / 7.0f;
+  public static final float TWO_NINTHS = 2.0f / 9.0f;
+  public static final float THREE_FOURTHS = 3.0f / 4.0f;
+  public static final float THREE_FIFTHS = 3.0f / 5.0f;
+  public static final float THREE_SEVENTHS = 3.0f / 7.0f;
+  public static final float THREE_EIGHTHS = 3.0f / 8.0f;
+  public static final float THREE_TENTHS = 3.0f / 10.0f;
+  public static final float FOUR_FIFTHS = 4.0f / 5.0f;
+  public static final float FOUR_SEVENTHS = 4.0f / 7.0f;
+  public static final float FOUR_NINTHS = 4.0f / 9.0f;
+  public static final float FIVE_SIXTHS = 5.0f / 6.0f;
+  public static final float FIVE_SEVENTHS = 5.0f / 7.0f;
+  public static final float FIVE_EIGHTHS = 5.0f / 8.0f;
+  public static final float FIVE_NINTHS = 5.0f / 9.0f;
+  public static final float SIX_SEVENTHS = 6.0f / 7.0f;
+  public static final float SEVEN_EIGHTHS = 7.0f / 8.0f;
+  public static final float SEVEN_NINTHS = 7.0f / 9.0f;
+  public static final float SEVEN_TENTHS = 7.0f / 10.0f;
+  public static final float EIGHT_NINTHS = 8.0f / 9.0f;
+  public static final float NINE_TENTHS = 9.0f / 10.0f;
+
+  private Constants ()
+  {
+    Classes.instantiationNotAllowed ();
+  }
+}

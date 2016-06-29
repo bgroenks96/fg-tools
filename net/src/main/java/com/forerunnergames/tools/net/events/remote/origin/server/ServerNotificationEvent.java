0901ba9fc86f8b76d7f3a1ced5334f646a33d056
Brian Groenke <brian@forerunnergames.com>
2016-06-29 19:58:42 -0400

    Add Broadcast/Group/Targeted notification events.
    See JavaDoc comments for a brief explanation on the event type definitions.

--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/ServerNotificationEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/ServerNotificationEvent.java
@@ -24,7 +24,7 @@
 package com.forerunnergames.tools.net.events.remote.origin.server;
 
 /**
- * Server is sending a one-way notification to the client.
+ * Server is sending a one-way notification to one or more clients.
  *
  * This event is intended to be sent over the network.
  */

f32e841cda318668e3abe97ea8b417a312a840c9
Brian Groenke <brian@forerunnergames.com>
2016-06-29 21:46:12 -0400

    Fix notification event types.
    - Fix notification event types not properly extending event target types.
    - Fix BroadcastNotificationEvent javadoc.

--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/BroadcastNotificationEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/BroadcastNotificationEvent.java
@@ -1,9 +1,10 @@
 package com.forerunnergames.tools.net.events.remote.origin.server;
 
 /**
- * A notification event sent from server to many
+ * A notification event sent from server to all clients.
  *
+ * This event is intended to be sent over the network.
  */
-public interface BroadcastNotificationEvent extends ServerNotificationEvent
+public interface BroadcastNotificationEvent extends ServerNotificationEvent, BroadcastEvent
 {
 }
--- net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/DirectNotificiationEvent.java
+++ net/src/main/java/com/forerunnergames/tools/net/events/remote/origin/server/DirectNotificiationEvent.java
@@ -1,10 +1,12 @@
 package com.forerunnergames.tools.net.events.remote.origin.server;
 
+import com.forerunnergames.tools.net.events.remote.DirectEvent;
+
 /**
  * A notification event sent from server to a single client.
  *
  * This event is intended to be sent over the network.
  */
-public interface DirectNotificiationEvent extends ServerNotificationEvent
+public interface DirectNotificiationEvent extends ServerNotificationEvent, DirectEvent
 {
 }

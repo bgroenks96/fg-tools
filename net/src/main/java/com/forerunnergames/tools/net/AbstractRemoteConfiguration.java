bgroenks96/fg-tools

commit a2ea57bc20a4a26f69ee9b01fbd1c25c0cd83ae4

Improve net classes (major API changes).
Summary:

- The goal of these changes is to provide a way to obtain the network
  configuration (address & port) of a Remote directly from itself,
  instead of having to get it from elsewhere. Sometimes the
  configuration just isn't available anywhere else, and all one has to
  work with is a Remote.

- As a side-effect of implementing the above goal, we get increased type
  safety as a result, but while we're at it, we'll further increase type
  safety as much as is practical / possible.

- Another minor goal is to decrease confusion through better naming,
  more javadoc, & some package reorganization.

- There are no event hierarchy changes or new event interfaces. The only
  event-related changes are extremely minor, and were necessary to
  accommodate the above changes.

- These changes cause major API changes (read: BREAK ALL THE THINGS!),
  and as such are going to trigger a release directly after being
  committed.

Details:

- Create RemoteClient & RemoteServer, which provide ClientConfiguration &
  ServerConfiguration, respectively, & extend Remote. Replace all usages of
  Remote where possible with these two subtypes. This change is the main
  purpose of this commit. Everything else is secondary. Not only does this
  increase type safety by allowing one to know immediately if a remote endpoint
  is a client or server, but more importantly, it allows obtaining the
  configuration, which contains it's address & port, without having to get it
  elsewhere. Sometimes the configuration just isn't available anywhere else,
  and all one has to work with is a Remote.

- Generify NetworkEvent to take and return the Remote subclass instead of only
  a Remote, which supports the main goal of passing around RemoteClient &
  RemoteServer to have access to their *Configuration's.

- Generify NetworkCommunicationEvent to take and return the Remote subclass
  instead of only a Remote for the sender, which supports the main goal of

net/src/main/java/com/forerunnergames/tools/net/AbstractRemoteConfiguration.java
@@ -1,6 +1,5 @@
 /*
- * Copyright © 2011 - 2013 Aaron Mahan
- * Copyright © 2013 - 2016 Forerunner Games, LLC
+ * Copyright © 2016 Forerunner Games, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,22 +20,47 @@
  * SOFTWARE.
  */
 
-package com.forerunnergames.tools.net.server;
+package com.forerunnergames.tools.net;
 
-import com.forerunnergames.tools.common.Result;
+import com.forerunnergames.tools.common.Arguments;
+import com.forerunnergames.tools.common.Strings;
+import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
 
-import java.util.concurrent.Future;
-
-public interface ServerConnector
+public abstract class AbstractRemoteConfiguration implements RemoteConfiguration
 {
-  Result <String> connectNow (final String address, final int tcpPort, final int timeoutMs, final int maxAttempts);
+  private final String address;
+  private final int port;
 
-  Future <Result <String>> connectLater (final String address,
-                                         final int tcpPort,
-                                         final int timeoutMs,
-                                         final int maxAttempts);
+  protected AbstractRemoteConfiguration (final String address, final int port)
+  {
+    Arguments.checkIsNotNull (address, "address");
+    Arguments.checkIsNotNegative (port, "port");
 
-  boolean isConnected ();
+    this.address = address;
+    this.port = port;
+  }
 
-  void disconnect ();
+  @RequiredForNetworkSerialization
+  protected AbstractRemoteConfiguration ()
+  {
+    address = null;
+    port = 0;
+  }
+
+  @Override
+  public String getAddress ()
+  {
+    return address;
+  }
+
+  public int getPort ()
+  {
+    return port;
+  }
+
+  @Override
+  public String toString ()
+  {
+    return Strings.format ("{}: Address: [{}] | Port: [{}]", getClass ().getSimpleName (), address, port);
+  }
 }

net/src/main/java/com/forerunnergames/tools/net/NetworkConstants.java
@@ -23,16 +23,9 @@
 
 package com.forerunnergames.tools.net;
 
-import com.forerunnergames.tools.common.Arguments;
 import com.forerunnergames.tools.common.Classes;
 
-import com.google.common.net.HostSpecifier;
-import com.google.common.net.InetAddresses;
-import com.google.common.net.InternetDomainName;
-
 import java.util.regex.Pattern;
-
-import javax.annotation.Nullable;
 
 public final class NetworkConstants
 {
@@ -45,28 +38,6 @@ public final class NetworkConstants
   public static final int MAX_SERVER_ADDRESS_STRING_LENGTH = 255;
   public static final int MIN_SERVER_PORT_STRING_LENGTH = 1;
   public static final int MAX_SERVER_PORT_STRING_LENGTH = 5;
-
-  public static boolean isLocalhostAddress (final String address)
-  {
-    Arguments.checkIsNotNull (address, "address");
-
-    return address.equals (LOCALHOST_ADDRESS) || address.equalsIgnoreCase (LOCALHOST_NAME);
-  }
-
-  public static boolean isValidIpAddress (@Nullable final String address)
-  {
-    return address != null && InetAddresses.isInetAddress (address);
-  }
-
-  public static boolean isValidAddress (@Nullable final String address)
-  {
-    return address != null && HostSpecifier.isValid (address);
-  }
-
-  public static boolean isValidDomainName (@Nullable final String address)
-  {
-    return address != null && InternetDomainName.isValid (address);
-  }
 
   private NetworkConstants ()
   {

net/src/main/java/com/forerunnergames/tools/net/NetworkTools.java
@@ -21,20 +21,44 @@
  * SOFTWARE.
  */
 
-package com.forerunnergames.tools.net.client.configuration;
+package com.forerunnergames.tools.net;
 
-import com.forerunnergames.tools.net.AbstractRemoteConfiguration;
-import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
+import com.forerunnergames.tools.common.Arguments;
+import com.forerunnergames.tools.common.Classes;
 
-public final class DefaultClientConfiguration extends AbstractRemoteConfiguration implements ClientConfiguration
+import com.google.common.net.HostSpecifier;
+import com.google.common.net.InetAddresses;
+import com.google.common.net.InternetDomainName;
+
+import javax.annotation.Nullable;
+
+public final class NetworkTools
 {
-  public DefaultClientConfiguration (final String clientAddress, final int clientPort)
+  public static boolean isLocalhostAddress (final String address)
   {
-    super (clientAddress, clientPort);
+    Arguments.checkIsNotNull (address, "address");
+
+    return address.equals (NetworkConstants.LOCALHOST_ADDRESS)
+            || address.equalsIgnoreCase (NetworkConstants.LOCALHOST_NAME);
   }
 
-  @RequiredForNetworkSerialization
-  private DefaultClientConfiguration ()
+  public static boolean isValidIpAddress (@Nullable final String address)
   {
+    return address != null && InetAddresses.isInetAddress (address);
+  }
+
+  public static boolean isValidAddress (@Nullable final String address)
+  {
+    return address != null && HostSpecifier.isValid (address);
+  }
+
+  public static boolean isValidDomainName (@Nullable final String address)
+  {
+    return address != null && InternetDomainName.isValid (address);
+  }
+
+  private NetworkTools ()
+  {
+    Classes.instantiationNotAllowed ();
   }
 }

net/src/main/java/com/forerunnergames/tools/net/RemoteConfiguration.java
@@ -1,6 +1,5 @@
 /*
- * Copyright © 2011 - 2013 Aaron Mahan
- * Copyright © 2013 - 2016 Forerunner Games, LLC
+ * Copyright © 2016 Forerunner Games, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,20 +20,11 @@
  * SOFTWARE.
  */
 
-package com.forerunnergames.tools.net.client.configuration;
+package com.forerunnergames.tools.net;
 
-import com.forerunnergames.tools.net.AbstractRemoteConfiguration;
-import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
-
-public final class DefaultClientConfiguration extends AbstractRemoteConfiguration implements ClientConfiguration
+public interface RemoteConfiguration
 {
-  public DefaultClientConfiguration (final String clientAddress, final int clientPort)
-  {
-    super (clientAddress, clientPort);
-  }
+  String getAddress ();
 
-  @RequiredForNetworkSerialization
-  private DefaultClientConfiguration ()
-  {
-  }
+  int getPort ();
 }

net/src/main/java/com/forerunnergames/tools/net/RemoteListener.java
@@ -1,6 +1,5 @@
 /*
- * Copyright © 2011 - 2013 Aaron Mahan
- * Copyright © 2013 - 2016 Forerunner Games, LLC
+ * Copyright © 2016 Forerunner Games, LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,20 +20,13 @@
  * SOFTWARE.
  */
 
-package com.forerunnergames.tools.net.client.configuration;
+package com.forerunnergames.tools.net;
 
-import com.forerunnergames.tools.net.AbstractRemoteConfiguration;
-import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
-
-public final class DefaultClientConfiguration extends AbstractRemoteConfiguration implements ClientConfiguration
+public interface RemoteListener <T extends Remote>
 {
-  public DefaultClientConfiguration (final String clientAddress, final int clientPort)
-  {
-    super (clientAddress, clientPort);
-  }
+  void connected (final T remote);
 
-  @RequiredForNetworkSerialization
-  private DefaultClientConfiguration ()
-  {
-  }
+  void disconnected (final T remote);
+
+  void received (final T sender, final Object object);
 }

net/src/main/java/com/forerunnergames/tools/net/client/AbstractClientController.java
@@ -26,8 +26,9 @@ package com.forerunnergames.tools.net.client;
 import com.forerunnergames.tools.common.Arguments;
 import com.forerunnergames.tools.common.Result;
 import com.forerunnergames.tools.common.controllers.ControllerAdapter;
-import com.forerunnergames.tools.net.NetworkListener;
-import com.forerunnergames.tools.net.Remote;
+import com.forerunnergames.tools.net.client.remote.RemoteServer;
+import com.forerunnergames.tools.net.client.remote.RemoteServerListener;
+import com.forerunnergames.tools.net.server.configuration.ServerConfiguration;
 
 import com.google.common.collect.ImmutableSet;
 
@@ -66,26 +67,25 @@ public abstract class AbstractClientController extends ControllerAdapter impleme
   }
 
   @Override
-  public Result <String> connectNow (final String address,
-                                     final int tcpPort,
-                                     final int timeoutMs,
-                                     final int maxAttempts)
+  public Result <String> connectNow (final ServerConfiguration config, final int timeoutMs, final int maxAttempts)
   {
-    Arguments.checkIsNotNull (address, "address");
-    Arguments.checkIsNotNegative (tcpPort, "tcpPort");
+    Arguments.checkIsNotNull (config, "config");
     Arguments.checkIsNotNegative (timeoutMs, "timeoutMs");
     Arguments.checkIsNotNegative (maxAttempts, "maxAttempts");
 
-    return client.connectNow (address, tcpPort, timeoutMs, maxAttempts);
+    return client.connectNow (config, timeoutMs, maxAttempts);
   }
 
   @Override
-  public Future <Result <String>> connectLater (final String address,
-                                                final int tcpPort,
+  public Future <Result <String>> connectLater (final ServerConfiguration config,
                                                 final int timeoutMs,
                                                 final int maxAttempts)
   {
-    return client.connectLater (address, tcpPort, timeoutMs, maxAttempts);
+    Arguments.checkIsNotNull (config, "config");
+    Arguments.checkIsNotNegative (timeoutMs, "timeoutMs");
+    Arguments.checkIsNotNegative (maxAttempts, "maxAttempts");
+
+    return client.connectLater (config, timeoutMs, maxAttempts);
   }
 
   @Override
@@ -108,11 +108,11 @@ public abstract class AbstractClientController extends ControllerAdapter impleme
     client.send (object);
   }
 
-  protected abstract void onConnectionTo (final Remote server);
+  protected abstract void onConnectionTo (final RemoteServer server);
 
-  protected abstract void onDisconnectionFrom (final Remote server);
+  protected abstract void onDisconnectionFrom (final RemoteServer server);
 
-  protected abstract void onCommunication (final Object object, final Remote server);
+  protected abstract void onCommunication (final RemoteServer server, final Object object);
 
   private void registerClasses ()
   {
@@ -124,31 +124,31 @@ public abstract class AbstractClientController extends ControllerAdapter impleme
 
   private void addListener ()
   {
-    client.add (new NetworkListener ()
+    client.add (new RemoteServerListener ()
     {
       @Override
-      public void connected (final Remote server)
+      public void connected (final RemoteServer remote)
       {
-        Arguments.checkIsNotNull (server, "server");
+        Arguments.checkIsNotNull (remote, "server");
 
-        onConnectionTo (server);
+        onConnectionTo (remote);
       }
 
       @Override
-      public void disconnected (final Remote server)
+      public void disconnected (final RemoteServer remote)
       {
-        Arguments.checkIsNotNull (server, "server");
+        Arguments.checkIsNotNull (remote, "server");
 
-        onDisconnectionFrom (server);
+        onDisconnectionFrom (remote);
       }
 
       @Override
-      public void received (final Object object, final Remote server)
+      public void received (final RemoteServer sender, final Object object)
       {
+        Arguments.checkIsNotNull (sender, "server");
         Arguments.checkIsNotNull (object, "object");
-        Arguments.checkIsNotNull (server, "server");
 
-        onCommunication (object, server);
+        onCommunication (sender, object);
       }
     });
   }

net/src/main/java/com/forerunnergames/tools/net/client/Client.java
@@ -23,16 +23,19 @@
 
 package com.forerunnergames.tools.net.client;
 
-import com.forerunnergames.tools.net.NetworkListener;
-import com.forerunnergames.tools.net.server.ServerCommunicator;
-import com.forerunnergames.tools.net.server.ServerConnector;
+import com.forerunnergames.tools.net.client.remote.RemoteServerCommunicator;
+import com.forerunnergames.tools.net.client.remote.RemoteServerConnector;
+import com.forerunnergames.tools.net.client.remote.RemoteServerListener;
 
-public interface Client extends ServerConnector, ServerCommunicator
+public interface Client extends RemoteServerConnector, RemoteServerCommunicator
 {
-  void add (final NetworkListener networkListener);
+  void add (final RemoteServerListener listener);
 
-  void remove (final NetworkListener networkListener);
+  void remove (final RemoteServerListener listener);
 
+  /**
+   * Registers a class for network serialization. Every class to be sent over the network should be registered.
+   */
   void register (final Class <?> type);
 
   void start ();

net/src/main/java/com/forerunnergames/tools/net/client/ClientController.java
@@ -24,9 +24,9 @@
 package com.forerunnergames.tools.net.client;
 
 import com.forerunnergames.tools.common.controllers.Controller;
-import com.forerunnergames.tools.net.server.ServerCommunicator;
-import com.forerunnergames.tools.net.server.ServerConnector;
+import com.forerunnergames.tools.net.client.remote.RemoteServerCommunicator;
+import com.forerunnergames.tools.net.client.remote.RemoteServerConnector;
 
-public interface ClientController extends Controller, ServerConnector, ServerCommunicator
+public interface ClientController extends Controller, RemoteServerConnector, RemoteServerCommunicator
 {
 }

commit 83d74355184d31baee6cf7c4567ca1bde951df19

Fix bug in AbstractRemoteConfiguration.
- Allow port to be negative. UnknownClientConfiguration &
  UnknownServerConfiguration both extend AbstractRemoteConfiguration,
  and pass in a port of -1, indicating no port. Other implementations
  may need to do the same thing. This is a regression caused by
  a2ea57bc20a4a26f69ee9b01fbd1c25c0cd83ae4.

- Make port negative in default serialization constructor for
  consistency. Never use 0 to indicate no port. Zero can be seen in some
  cases as a wildcard port.

net/src/main/java/com/forerunnergames/tools/net/AbstractRemoteConfiguration.java
@@ -24,17 +24,18 @@ package com.forerunnergames.tools.net;
 
 import com.forerunnergames.tools.common.Arguments;
 import com.forerunnergames.tools.common.Strings;
+import com.forerunnergames.tools.common.annotations.AllowNegative;
 import com.forerunnergames.tools.net.annotations.RequiredForNetworkSerialization;
 
 public abstract class AbstractRemoteConfiguration implements RemoteConfiguration
 {
   private final String address;
+  @AllowNegative
   private final int port;
 
-  protected AbstractRemoteConfiguration (final String address, final int port)
+  protected AbstractRemoteConfiguration (final String address, @AllowNegative final int port)
   {
     Arguments.checkIsNotNull (address, "address");
-    Arguments.checkIsNotNegative (port, "port");
 
     this.address = address;
     this.port = port;
@@ -44,7 +45,7 @@ public abstract class AbstractRemoteConfiguration implements RemoteConfiguration
   protected AbstractRemoteConfiguration ()
   {
     address = null;
-    port = 0;
+    port = -1;
   }
 
   @Override